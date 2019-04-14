b1cc582979af712ade0386556aabb67d97bf76f2
Ricky Severino <rickyseverino@protonmail.com>
2019-04-14 17:01:23 -0400

    add speed test comparison btw. cpp and julia

---
+++ SpeedTest/square.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int square(int x) {
+    return x*x;
+}
+
+int main() {
+
+    std::cout << "4 * 4 = " << square(4) << std::endl;
+
+    return 0;
+}
---
+++ SpeedTest/square.jl
@@ -0,0 +1,5 @@
+function square(x)
+    return x*x
+end
+
+println("4 * 4 = ", square(4))
