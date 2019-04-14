bce513fa214242c83249ca21cd46644e62ddd4b9
Ricky Severino <rickyseverino@protonmail.com>
2019-04-13 23:53:27 -0400

    add simple calculus tutorial

---
+++ calculus.jl
@@ -0,0 +1,21 @@
+using Calculus
+using QuadGK
+
+# symbolic differentiation
+
+println("\nThe derivative of sin(x) :")
+println(differentiate("sin(x)", :x))
+
+println("\nTher derivative of 2 * x^2 :")
+println(differentiate("2x^2", :x))
+
+println("\nThe derivative of x * sin(x^2) + 1 :")
+println(differentiate("x*sin(x^2)+1", :x))
+
+# integration
+
+println("\nThe integral of 2/(1-x) from -1 to 0 :")
+
+f(x) = 2/(1-x)
+println(quadgk(f, -1, 0))
+
